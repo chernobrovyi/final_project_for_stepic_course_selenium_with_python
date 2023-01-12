ae09311c8cab84a0e70aea417e2062821a04b18b
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2023-01-12 21:46:02 +0300

    add metod should_be_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -7,18 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-#    def test_shop_item(self, browser):
-#        link =  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/";
-#        browser.get(link);
-#        time.sleep(5);
-#
-#        btn_basket_add = browser.find_element(By.CLASS_NAME, "btn-add-to-basket");
-#        btn_basket_add.click();
-#        time.sleep(5);
-#
-#        alert_window = browser.find_element(By.CLASS_NAME, "alertinner ");
-#        alert_window_text = alert_window.text;
-#
-#        assert "Coders at Work has been added to your basket." == alert_window_text;
-
-# не забываем оставить пустую строку в конце файла
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
